93e8b409178351113ee98585b87c456899d86bd7
wangyuanjie <antony_wong@msn.com>
2017-09-08 16:45:36 +0800

    增加babel代码

--- 前端/Webpack/Example/app/Greeter.js
+++ 前端/Webpack/Example/app/Greeter.js
@@ -1,5 +1,23 @@
-module.exports = function () {
-    var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!!";
-    return greet;
-};
+// var config = require('./config.json');
+
+// module.exports = function () {
+//     var greet = document.createElement('div');
+//     greet.textContent = config.greetText;
+//     return greet;
+// };
+
+import React, { Component } from 'react'
+import config from './config.json';
+import styles from './Greeter.css';
+
+class Greeter extends Component {
+    render() {
+        return (
+            <div className={styles.root}>
+                {config.greetText}
+            </div>
+        );
+    }
+}
+
+export default Greeter
